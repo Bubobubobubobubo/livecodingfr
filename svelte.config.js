8b64ce7825045c40717b3a55ffd5ccd61ff3e3ad
Raphael Forment <raphael.forment@gmail.com>
2023-11-04 13:09:08 +0100

    revert change

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({ pages: "dist" }),
     alias: {
-      $data: "./src/data",
+      $data: "src/data",
     }
   },
 };
