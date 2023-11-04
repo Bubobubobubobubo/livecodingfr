ff39b1197d6d45ba0d642e4cd5644c8668d96be3
Raphael Forment <raphael.forment@gmail.com>
2023-11-04 13:14:59 +0100

    trying some weird move from stack overflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
+
 
 export default defineConfig({
   base: '.',
+  resolve: {
+    alias: {
+      '$': path.resolve(__dirname, './src'),
+      '$lib': path.resolve(__dirname, './src/lib'),
+      '$data': path.resolve(__dirname, './src/data'),
+    },
+  },
   root: 'src',
   plugins: [sveltekit()],
   build: {
