4cf6b72b75723d664c14d8aef1bcf87d2e31da12
Raphael Forment <raphael.forment@gmail.com>
2024-01-03 13:34:59 +0100

    texte plus petit dans composant info

--- src/lib/components/Info.svelte
+++ src/lib/components/Info.svelte
@@ -27,5 +27,5 @@
       d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
     /></svg
   >
-  <span class="text-2xl">{@html info}</span>
+  <span class="text-xl">{@html info}</span>
 </div>
