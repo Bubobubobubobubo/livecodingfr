f5867393b69e7dcc84519b6fca8362aaf6e80296
Bubobubobubobubo <Bubobubobubobubo@users.noreply.github.com>
2024-02-09 23:58:26 +0000

    deploy: e497183ca3c0906d4d201f327322aaeeb2fd568a

---
+++ _app/immutable/nodes/14.b3JyxHYv.js
@@ -0,0 +1,21 @@
+import{s as M,n as P}from"../chunks/scheduler.aZRR5A_O.js";import{S as T,i as j,s as m,g as d,r as R,B as S,f as s,c as p,h as f,x as h,u as y,a as l,v as z,d as F,t as I,w as U}from"../chunks/index.kYoizZ4A.js";import{I as k}from"../chunks/Info.ITY44f4q.js";function B(H){let c,n,L="Ressources",v,o,g,i,w="Ressources anglophones",_,r,q=`<li><p><a href="https://toplap.org" rel="nofollow">TOPLAP</a> : <strong>TOPLAP</strong> est le collectif historique qui a
+promu la pratique du <em>live coding</em> à partir de sa fondation en 2004. Le
+collectif est toujours actif et a aujourd’hui évolué pour constituer un grand
+réseau international de chercheurs et d’artistes travaillant autour de la
+question du <em>live coding</em> dans l’ensemble les arts (son, visuels, chorégraphie,
+etc).</p></li> <li><p><a href="https://algorave.com" rel="nofollow">Algorave</a> : un site de recensement des <em>Algoraves</em>,
+pour <em>Algorithmic Rave Parties</em>. Mouvement d’évènements lancé en 2012 par Nick
+Collins et Alex McLean, pour une pratique du <em>live coding</em> dans un contexte
+festif, en imitation des <em>rave parties</em> britanniques.</p></li> <li><p><a href="https://github.com/toplap/awesome-livecoding" rel="nofollow">Awesome Live Coding</a> : Une
+longue liste de ressources liées au <em>live coding</em> : logiciels, liens, sites,
+archives, etc. La liste est maintenue de longue date et elle reste active. Vous trouverez
+beaucoup de choses intéressantes là-bas.</p></li> <li><p><a href="https://llllllll.co/" rel="nofollow">Lines Forum</a> : initialement, le forum de la communauté
+<strong>Monome</strong>. Ce forum accueille toutefois beaucoup de musiciens et d’artistes
+intéressés par la programmation et le <strong>DIY</strong>. Beaucoup de choses intéressantes
+pour le <em>live coding</em>.</p></li> <li><p><a href="https://scsynth.org/" rel="nofollow">SuperCollider Forum</a> : le forum officiel dédié à
+<strong>SuperCollider</strong>. <strong>SuperCollider</strong> est en grande partie responsable de
+l’émergence de la scène musicale du <em>live coding</em>, et pour de bonnes raisons.
+Foisonnant, infini, logiciel tentaculaire et puissant.</p></li>`,x,a,A="Ressources francophones",C,u,$='<li><a href="https://ressources.labomedia.org/live_coding" rel="nofollow">Labomedia</a> : collection de ressources pour le <em>creative coding</em>.</li>',b;return o=new k({props:{info:`Cette page est une liste de ressources généralistes sur le <i>live
+coding</i>. Au vu de l'objectif poursuivi par ce site, il est préférable de 
+mentionner les sources francophones en premier puis d'inclure des liens 
+généralistes.`,markdown:"false"}}),{c(){c=m(),n=d("h1"),n.textContent=L,v=m(),R(o.$$.fragment),g=m(),i=d("h2"),i.textContent=w,_=m(),r=d("ul"),r.innerHTML=q,x=m(),a=d("h2"),a.textContent=A,C=m(),u=d("ul"),u.innerHTML=$,this.h()},l(e){S("svelte-1q3gdtb",document.head).forEach(s),c=p(e),n=f(e,"H1",{"data-svelte-h":!0}),h(n)!=="svelte-1cbhex0"&&(n.textContent=L),v=p(e),y(o.$$.fragment,e),g=p(e),i=f(e,"H2",{"data-svelte-h":!0}),h(i)!=="svelte-1n0zz4i"&&(i.textContent=w),_=p(e),r=f(e,"UL",{"data-svelte-h":!0}),h(r)!=="svelte-1lzqyun"&&(r.innerHTML=q),x=p(e),a=f(e,"H2",{"data-svelte-h":!0}),h(a)!=="svelte-hrdjwm"&&(a.textContent=A),C=p(e),u=f(e,"UL",{"data-svelte-h":!0}),h(u)!=="svelte-1obfvjx"&&(u.innerHTML=$),this.h()},h(){document.title="Ressources"},m(e,t){l(e,c,t),l(e,n,t),l(e,v,t),z(o,e,t),l(e,g,t),l(e,i,t),l(e,_,t),l(e,r,t),l(e,x,t),l(e,a,t),l(e,C,t),l(e,u,t),b=!0},p:P,i(e){b||(F(o.$$.fragment,e),b=!0)},o(e){I(o.$$.fragment,e),b=!1},d(e){e&&(s(c),s(n),s(v),s(g),s(i),s(_),s(r),s(x),s(a),s(C),s(u)),U(o,e)}}}class N extends T{constructor(c){super(),j(this,c,null,B,M,{})}}export{N as component};
