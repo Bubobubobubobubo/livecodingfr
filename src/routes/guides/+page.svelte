5abcd593da8d09d206767d9edf55e3d7d9c04cd9
Raphael Forment <raphael.forment@gmail.com>
2024-02-09 10:16:00 +0100

    Fix: article CSS works better in responsive mode

--- src/routes/guides/+page.svelte
+++ src/routes/guides/+page.svelte
@@ -15,14 +15,17 @@
 <ul>
   {#each data.guides as post}
     <li
-      class="list-none px-4 pb-4 my-4 rounded-lg bg-base-300 dark:bg-base-300 hover:bg-base-200 hover:dark:bg-base-200 border-1 border-color-base-100"
+      class="ml-0 list-none px-4 pb-4 my-4 rounded-lg bg-base-300 dark:bg-base-300 hover:bg-base-200 hover:dark:bg-base-200 border-1 border-color-base-100"
     >
       <h2>
         <a href={post.path}>
           {post.meta.title}
         </a>
       </h2>
-      <p class="pt-2">Publié le : {post.meta.date} par {post.meta.author}</p>
+      <p class="pt-2">
+        <b>Publié le :</b>
+        {post.meta.date} par <i>{post.meta.author}</i>
+      </p>
     </li>
   {/each}
 </ul>
