d90ecfccd033e98b9def12a6ad9a9f5be37ff75b
Raphaël Forment <raphael.forment@gmail.com>
2025-08-30 15:46:21 +0200

    Much better .gitignore file, dist was tracked for some reason

--- dist/_app/immutable/chunks/DmO46a85.js
+++
@@ -1,1 +0,0 @@
-const e=o=>{for(let t=o.length-1;t>0;t--){const s=Math.floor(Math.random()*(t+1));[o[t],o[s]]=[o[s],o[t]]}};export{e as s};
--- dist/_app/immutable/entry/start.DE5tvq6w.js
+++
@@ -1,1 +0,0 @@
-import{Z as o,Y as r}from"../chunks/CA-i7br4.js";export{o as load_css,r as start};
